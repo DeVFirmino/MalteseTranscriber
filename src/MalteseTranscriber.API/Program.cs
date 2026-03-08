cb5df5fabe586bc46f9231465403e1d600b4ca26
devfirmino <daanspfc@gmail.com>
2026-03-08 22:41:28 +0100

    fix: revert EnableDetailedErrors to dev-only

--- src/MalteseTranscriber.API/Program.cs
+++ src/MalteseTranscriber.API/Program.cs
@@ -38,7 +38,7 @@ try
     builder.Services.AddSignalR(opts =>
     {
         opts.MaximumReceiveMessageSize = 1_048_576;
-        opts.EnableDetailedErrors = true;
+        opts.EnableDetailedErrors = builder.Environment.IsDevelopment();
     });
 
     builder.Services.AddMemoryCache();
